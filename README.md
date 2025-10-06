d2579e9044d3d9b2f4125e00962f2347e49e870b
Justus0405 <justus040506@gmail.com>
2025-10-06 20:24:46 +0200

    Added files via upload

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
+<p align="left">
+    <!-- Discord Badge -->
+    <a href="https://discord.justus0405.com/"><img src="https://img.shields.io/discord/1370519315400495234?logo=Discord&colorA=1e1e2e&colorB=a6e3a1&style=for-the-badge"></a>
+    <!-- Version Badge -->
+    <a href="https://github.com/Justus0405/Btfy/blob/main/Btfy"><img src="https://img.shields.io/badge/Version-1.0-blue?colorA=1e1e2e&colorB=cdd6f4&style=for-the-badge"></a>
+</p>
+
+<p align="left">
+    <!-- Stars Badge -->
+	<a href="https://github.com/Justus0405/Btfy/stargazers"><img src="https://img.shields.io/github/stars/Justus0405/Btfy?colorA=1e1e2e&colorB=b7bdf8&style=for-the-badge"></a>
+    <!-- Issues Badge -->
+	<a href="https://github.com/Justus0405/Btfy/issues"><img src="https://img.shields.io/github/issues/Justus0405/Btfy?colorA=1e1e2e&colorB=f5a97f&style=for-the-badge"></a>
+    <!-- Contributors Badge -->
+	<a href="https://github.com/Justus0405/Btfy/contributors"><img src="https://img.shields.io/github/contributors/Justus0405/Btfy?colorA=1e1e2e&colorB=a6da95&style=for-the-badge"></a>
+</p>
+
 # Btfy
-🔋 Termux-API Battery Monitoring via Discord Webhook
+
+Btfy is a small Bash script made for Termux. It checks your device battery status and sends updates to a Discord channel using a webhook.
+
+> [!TIP]
+> If you are experiencing sudden breakge of the script,
+> disable deep sleep for termux using `termux-wake-lock`.
+
+## Features
+
+- Full Notification
+- Charging Notification
+- Discharging Notification
+- Low Battery Notification
+- Additional info like Temperature and Health
+
+## Dependencies
+
+```plaintext
+termux-api
+jq
+curl
+```
+
+## Installation
+
+1. Clone the repository:
+
+```shell
+git clone https://github.com/Justus0405/Btfy.git
+```
+
+2. Navigate to the directory:
+
+```shell
+cd Btfy
+```
+
+3. Make the script executable:
+
+```shell
+chmod +x btfy.sh
+```
+
+4. Edit the script to configure the webhook url and check interval:
+
+```shell
+nano btfy.sh
+```
+
+5. Run the script:
+
+```shell
+./btfy.sh
+```
+
+## Usage
+
+```plaintext
+usage: btfy.sh [...]
+arguments:
+	    start
+	    stop
+	    help
+	    version
+```
+
+#
+
+<p align="center">
+	Copyright &copy; 2025-present <a href="https://github.com/Justus0405" target="_blank">Justus0405</a>
+</p>
+
+<p align="center">
+	<a href="https://github.com/Justus0405/Btfy/blob/main/LICENSE"><img src="https://img.shields.io/github/license/Justus0405/Btfy?logo=Github&colorA=1e1e2e&colorB=cba6f7&style=for-the-badge"></a>
+</p>
---
+++ btfy.sh
@@ -0,0 +1,263 @@
+#!/usr/bin/env bash
+#
+# Termux-API Battery Monitoring via Discord Webhook
+#
+# Author: Justus0405
+# Date: 05.09.2025
+# License: MIT
+
+export scriptVersion="1.0"
+
+## USER CONFIGURATION START
+
+# Discord webhook
+export WEBHOOK_URL=""
+
+# Interval for battery check
+export INTERVAL="60s"
+
+## USER CONFIGURATION STOP
+
+### COLOR CODES ###
+export black="\e[1;30m"
+export red="\e[1;31m"
+export green="\e[1;32m"
+export yellow="\e[1;33m"
+export blue="\e[1;34m"
+export purple="\e[1;35m"
+export cyan="\e[1;36m"
+export lightGray="\e[1;37m"
+export gray="\e[1;90m"
+export lightRed="\e[1;91m"
+export lightGreen="\e[1;92m"
+export lightYellow="\e[1;93m"
+export lightBlue="\e[1;94m"
+export lightPurple="\e[1;95m"
+export lightCyan="\e[1;96m"
+export white="\e[1;97m"
+export bold="\e[1m"
+export faint="\e[2m"
+export italic="\e[3m"
+export underlined="\e[4m"
+export blinking="\e[5m"
+export reset="\e[0m"
+
+### FUNCTIONS ###
+logMessage() {
+    local type="$1"
+    local message="$2"
+    case "${type}" in
+    "info" | "INFO")
+        echo -e "[  ${cyan}INFO${reset}  ] ${message}"
+        ;;
+    "done" | "DONE")
+        echo -e "[  ${green}DONE${reset}  ] ${message}"
+        exit 0
+        ;;
+    "warning" | "WARNING")
+        echo -e "[ ${red}FAILED${reset} ] ${message}"
+        ;;
+    "error" | "ERROR")
+        echo -e "[  ${red}ERROR${reset} ] ${message}"
+        exit 1
+        ;;
+    *)
+        echo -e "[UNDEFINED] ${message}"
+        ;;
+    esac
+}
+
+getArguments() {
+    case "$1" in
+    "start")
+        checkEnviroment
+        getEnviroment
+        startScript
+        ;;
+    "stop")
+        stopScript
+        ;;
+    "help")
+        printHelp
+        ;;
+    "version")
+        printVersion
+        ;;
+    "")
+        logMessage "error" "No operation specified. Use $(basename "$0") help"
+        ;;
+    *)
+        logMessage "error" "Unrecognized option '$1'. Use $(basename "$0") help"
+        ;;
+    esac
+}
+
+checkEnviroment() {
+    # Simple check for looking if all dependencies are installed and if a webhook is defined.
+
+    command -v curl >/dev/null 2>&1 || logMessage "error" "curl is not installed."
+    command -v jq >/dev/null 2>&1 || logMessage "error" "jq is not installed."
+
+    if [ -z "${WEBHOOK_URL}" ]; then
+        logMessage "error" "Missing discord webhook url."
+    fi
+}
+
+getEnviroment() {
+    # Tries to get the manufacturer and model name of the device.
+    # Used as Title for sendMessage().
+
+    manufacturer=$(getprop ro.product.manufacturer)
+    model=$(getprop ro.product.model)
+
+    # Fallback
+    if [ -z "${manufacturer}" ]; then
+        manufacturer=$(getprop ro.vendor.product.manufacturer)
+    fi
+
+    # Fallback
+    if [ -z "${model}" ]; then
+        model=$(getprop ro.vendor.product.model)
+    fi
+
+    if [ -z "${manufacturer}" ]; then
+        manufacturer="Unknown"
+    fi
+
+    if [ -z "${model}" ]; then
+        model="Unknown"
+    fi
+
+    # Neat trick with ^ which makes the first letter capital.
+    export embedName="${manufacturer^} ${model}"
+}
+
+startScript() {
+
+    logMessage "info" "Started battery notifier"
+    export lastChargingState=""
+
+    while :; do
+        # shellcheck disable=SC2155,SC2030
+        export chargingState=$(termux-battery-status | jq -r '.status')
+        # shellcheck disable=SC2155,SC2030
+        export batteryPercentage=$(termux-battery-status | jq -r '.percentage')
+
+        # When the current charging state differs from the last charging state,
+        # send a message announcing the new state.
+        if [[ "${chargingState}" != "${lastChargingState}" ]]; then
+
+            if [[ "${chargingState}" == "FULL" ]]; then
+                # shellcheck disable=SC2155,SC2030
+                export embedTitle="Your Device is fully charged!"
+                # shellcheck disable=SC2155,SC2030
+                export embedColor="16376495"
+                getBatteryInfo
+            fi
+
+            if [[ "${chargingState}" == "CHARGING" ]]; then
+                export embedTitle="Your Device is Charging!"
+                export embedColor="10937249"
+                getBatteryInfo
+            fi
+
+            if [[ "${chargingState}" == "DISCHARGING" ]]; then
+                export embedTitle="Your Device is Discharging!"
+                export embedColor="15961000"
+                getBatteryInfo
+            fi
+
+        fi
+
+        # Sends a low battery warning when at 15%.
+        if [[ "${sendPercentageNotification}" == false ]]; then
+
+            if [[ ${batteryPercentage} -lt 16 ]]; then
+                export sendPercentageNotification=true
+                export embedTitle="Your Device needs to be charged!"
+                export embedColor="16429959"
+                getBatteryInfo
+            fi
+
+        fi
+
+        # Unlocks the low battery warning at 17% again.
+        if [[ ${batteryPercentage} -gt 16 ]]; then
+            export sendPercentageNotification=false
+        fi
+
+        sleep ${INTERVAL}
+    done &
+}
+
+stopScript() {
+    logMessage "info" "Stopping battery notifier..."
+    pkill -f "sleep ${INTERVAL}" 2>&1
+    pkill -f "$(basename "$0")" 2>&1
+}
+
+getBatteryInfo() {
+    # Reads the json of termux-battery-status and format every entry to have
+    # a newline at the end, this makes for a dynamic info message depending on
+    # the battery model.
+
+    export embedMessage=""
+    export lastChargingState=${chargingState}
+
+    while IFS= read -r line; do
+        export embedMessage+="$line"$'\n'
+    done < <(termux-battery-status | jq -r 'to_entries[] | "\(.key): \(.value)"')
+
+    sendMessage
+}
+
+sendMessage() {
+    currentDatetime=$(date "+%d.%m.%Y %H:%M:%S")
+    message=$(jq -n \
+        --arg username "${embedName}" \
+        --arg avatar_url "https://emoji.aranja.com/emojis/google/1f50c.png" \
+        --arg title "${embedTitle}" \
+        --arg description "${embedMessage}" \
+        --argjson color "${embedColor}" \
+        --arg footer "Last checked: ${currentDatetime}" \
+        '{
+            username: $username,
+            avatar_url: $avatar_url,
+            embeds: [
+              {
+                title: $title,
+                description: $description,
+                color: $color,
+                footer: {
+                  text: $footer
+                }
+              }
+            ]
+          }')
+
+    curl -H "Content-Type: application/json" \
+        -X POST \
+        -d "${message}" \
+        "${WEBHOOK_URL}"
+}
+
+printHelp() {
+    echo -e "usage: $(basename "$0") [...]"
+    echo -e "arguments:"
+    echo -e "\t start"
+    echo -e "\t stop"
+    echo -e "\t help"
+    echo -e "\t version"
+    echo -e ""
+    exit 0
+}
+
+printVersion() {
+    echo -e "               $(basename "$0") v${scriptVersion} - GNU bash, version 5.3"
+    echo -e "               Copyright (C) 2025-present Justus0405"
+    echo -e ""
+    exit 0
+}
+
+### PROGRAM START ###
+getArguments "$@"
